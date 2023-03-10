2f0d83639ba9daf4e95f86faabd75807459443fc
helbing <helbingxxx@gmail.com>
2023-03-10 19:23:15 +0800

    feature: generator

--- examples/generator-example/src/app/index.ts
+++ examples/generator-example/src/app/index.ts
@@ -1,15 +1,55 @@
-import Generator from "yeoman-generator"
+import Generator, {
+  GeneratorOptions as BaseGeneratorOptions,
+} from "yeoman-generator"
+import chalk from "chalk"
 
-type GeneratorOptions = {
+export type IAnswers = {
+  // Example name
+  name: string
+}
+
+export type GeneratorOptions = BaseGeneratorOptions & {
+  // Example base path
+  basePath: string
+
+  // Example name
   name: string
 }
 
 export default class extends Generator<GeneratorOptions> {
   constructor(args: string | string[], options: GeneratorOptions) {
     super(args, options)
+
+    this.desc("Generate a example.")
+    this.argument("basePath", {
+      description: "Create example base path",
+      required: false,
+      default: "examples",
+    })
   }
 
-  foo() {
-    this.log("Hello world 123")
+  async prompting() {
+    return this.prompt<IAnswers>([
+      {
+        type: "input",
+        name: "name",
+        default: "example",
+        message: "Enter example name:",
+      },
+    ]).then((answers: IAnswers) => {
+      this.options.name = answers.name
+    })
+  }
+
+  async writing() {
+    this.sourceRoot()
+  }
+
+  async install() {
+    console.log("installing")
+  }
+
+  async end() {
+    this.log(chalk.yellow("Thanks for using the generator!"))
   }
 }
