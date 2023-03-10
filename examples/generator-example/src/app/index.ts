5034f36ae3626810db206f431f7452c0c1d758fc
helbing <helbingxxx@gmail.com>
2023-03-10 18:04:52 +0800

    chore: update

--- examples/generator-example/src/app/index.ts
+++ examples/generator-example/src/app/index.ts
@@ -1,3 +1,15 @@
-import { GeneratorApp } from "./generator-app"
+import Generator from "yeoman-generator"
 
-module.exports = GeneratorApp
+type GeneratorOptions = {
+  name: string
+}
+
+export default class extends Generator<GeneratorOptions> {
+  constructor(args: string | string[], options: GeneratorOptions) {
+    super(args, options)
+  }
+
+  foo() {
+    this.log("Hello world 123")
+  }
+}
